4d02babd49b37d4131231f826c24e5fa1303fe38
Bimsara_Kumarasekara <bkumarasekara80@gmail.com>
2026-03-30 23:48:46 +0530

    Create the Repository UserRepository.java

---
+++ server/src/main/java/com/smartcampus/api/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.smartcampus.api.repositories;
+
+import com.smartcampus.api.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    
+    // Spring Boot is smart enough to write the query for this just based on the method name!
+    Optional<User> findByEmail(String email);
+    
+    Optional<User> findByOauthId(String oauthId);
+}

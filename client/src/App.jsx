79ca46a8c65c3ae2aca66fafac8667a1fe346f08
AsiriTenne <asirib.tennakoon@gmail.com>
2026-04-02 21:59:02 +0530

    Wire resource booking pages into client routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
 import Dashboard from './pages/Dashboard';
 
+import ResourceList from './pages/ResourceList';
+import ResourceForm from './pages/ResourceForm';
+import ResourceDetails from './pages/ResourceDetails';
+
 function App() {
   return (
     <div>
       <Dashboard />
+      <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<ResourceList />} />
+          <Route path="/resources/add" element={<ResourceForm />} />
+          <Route path="/resources/:id" element={<ResourceDetails />} />
+          <Route path="/resources/:id/edit" element={<ResourceForm />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Layout>
+    </Router>
     </div>
+    
   );
 }
 
